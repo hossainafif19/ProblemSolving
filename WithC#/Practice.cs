a4855a8bee89934713bd84440a7af96fed9b6d26
Afif Hossain <137039631+hossainafif19@users.noreply.github.com>
2026-03-16 15:01:16 +0600

    ........

--- WithC#/Practice.cs
+++ WithC#/Practice.cs
@@ -1,15 +1,18 @@
-short[] array = { 1, 2, 4, 4, 4, 6, 6, 8 };
-short target = 4;
-short result = -1;
+//!               0  1  3  3  4  5  6  7
+short[] array = { 2, 2, 4, 4, 4, 6, 6, 8 };
+short target = 6;
 
-result = LowerBound(array, target);
-Console.WriteLine(result);
+short upperResult = UpperBound(array, target);
+Console.WriteLine("Upper: " + upperResult);
 
-short LowerBound(short[] arr, short target)
+short lowerResult = LowerBound(array, target);
+Console.WriteLine("Lower: " + lowerResult);
+
+short UpperBound(short[] arr, short target)
 {
     byte left = 0;
     byte right = (byte)(arr.Length - 1);
-    short result = -1;
+    short result = (short)(array.Length);
 
     while (left <= right)
     {
@@ -19,10 +22,33 @@ short LowerBound(short[] arr, short target)
         {
             result = mid;
             right = (byte)(mid - 1);
-
         }
         else
             left = (byte)(mid + 1);
+
+    }
+
+    return result;
+}
+
+short LowerBound(short[] arr, short target)
+{
+    byte left = 0;
+    byte right = (byte)(arr.Length - 1);
+    short result = (short)(array.Length);
+
+    while (left <= right)
+    {
+        byte mid = (byte)(left + (right - left) / 2);
+
+        if (arr[mid] >= target)
+        {
+            result = mid;
+            right = (byte)(mid - 1);
+        }
+        else
+            left = (byte)(mid + 1);
+
     }
 
     return result;
